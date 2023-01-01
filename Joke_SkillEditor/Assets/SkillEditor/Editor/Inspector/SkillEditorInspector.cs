31a59523ced5b60022047f30c33d949d2f3dcb9a
Youwant <971791748@qq.com>
2023-01-01 14:11:12 +0800

    6.2.动画片段的Inspector绘制

--- Joke_SkillEditor/Assets/SkillEditor/Editor/Inspector/SkillEditorInspector.cs
+++ Joke_SkillEditor/Assets/SkillEditor/Editor/Inspector/SkillEditorInspector.cs
@@ -51,7 +51,7 @@ public class SkillEditorInspector : Editor
     {
         if (root != null)
         {
-            for (int i = 0; i < root.childCount; i++)
+            for (int i = root.childCount - 1; i >= 0; i--)
             {
                 root.RemoveAt(i);
             }
@@ -61,10 +61,37 @@ public class SkillEditorInspector : Editor
 
     private void DrawAnimationTrackItem(AnimationTrackItem animationTrackItem)
     {
+        //动画资源
         ObjectField animationClipAssetField = new ObjectField("动画资源");
         animationClipAssetField.objectType = typeof(AnimationClip);
         animationClipAssetField.value = animationTrackItem.AnimationEvent.AnimationClip;
         root.Add(animationClipAssetField);
+
+        //轨道长度
+        IntegerField duration = new IntegerField("轨道长度");
+        duration.value = animationTrackItem.AnimationEvent.DurationFrame;
+        root.Add(duration);
+
+        //过渡时间
+        FloatField transitionTime = new FloatField("过渡时间");
+        transitionTime.value = animationTrackItem.AnimationEvent.TransitionTime;
+        root.Add(transitionTime);
+
+        //动画相关的信息
+        int clipFrameCount = (int)(animationTrackItem.AnimationEvent.AnimationClip.length * animationTrackItem.AnimationEvent.AnimationClip.frameRate);
+        Label clipFrame = new Label("动画资源长度：" + clipFrameCount);
+        root.Add(clipFrame);
+
+        Label isLoopLable = new Label("循环动画：" + animationTrackItem.AnimationEvent.AnimationClip.isLooping);
+        root.Add(isLoopLable);
+
+        //删除
+        Button deleteButton = new Button();
+        deleteButton.text = "删除";
+        deleteButton.style.backgroundColor = new Color(1, 0, 0, 0.5f);
+        root.Add(deleteButton);
+
+
 
     }
 
